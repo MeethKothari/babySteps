a900c820dd50f56128b9a4869480e460c772b33a
Meeth <meethukothari@gmail.com>
2025-02-21 13:17:37 +0530

    added cors option to cors policy

--- frontend/src/ipconfig.js
+++ frontend/src/ipconfig.js
@@ -1,3 +1,3 @@
-//const url = 'http://localhost:8082'; // local api server
-const url = "https://babysteps-khws.onrender.com"; // render api server
+// const url = 'http://localhost:8082'; // local api server
+const url = "https://babysteps-khws.onrender.com"; // render api server 
 export default url;
